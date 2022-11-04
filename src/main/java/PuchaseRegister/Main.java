c84df019c623af8d0c8085ff2b5367de85183292
lgrimm1 <laszlo.grimm1@gmail.com>
2022-11-04 18:52:44 +0400

    Initial commit.

---
+++ src/main/java/PuchaseRegister/Main.java
@@ -0,0 +1,11 @@
+package PuchaseRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //marks that this is the launcher class
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
